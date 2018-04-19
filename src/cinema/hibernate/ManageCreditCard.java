5dfacb225240042e519fab6edf7dbeff243fdd1c
rzd83091 <richard.z.deng@gmail.com>
2018-04-18 20:38:05 -0400

    getting more stuff

--- src/cinema/hibernate/ManageCreditCard.java
+++ src/cinema/hibernate/ManageCreditCard.java
@@ -131,4 +131,30 @@ public class ManageCreditCard {
 		
 		
 	}
+	
+	public boolean isValid(String ccNumber) {
+			//CreditCard cc = (CreditCard)session.createQuery("from CreditCard c where c.creditCardNo = " + ccNumber);
+			
+			int[] ints = new int[ccNumber.length()];
+			for (int i = 0; i < ccNumber.length(); i++) {
+				ints[i] = Integer.parseInt(ccNumber.substring(i, i + 1));
+			}
+			for (int i = ints.length - 2; i >= 0; i = i - 2) {
+				int j = ints[i];
+				j = j * 2;
+				if (j > 9) {
+					j = j % 10 + 1;
+				}
+				ints[i] = j;
+			}
+			int sum = 0;
+			for (int i = 0; i < ints.length; i++) {
+				sum += ints[i];
+			}
+			if (sum % 10 == 0) {
+				return true;
+			}
+			
+		return false;
+	}
 }
